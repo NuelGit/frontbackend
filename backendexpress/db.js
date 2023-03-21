c6719b4796493ff393549b1c9c8e4e3d2c17fcd2
NuelGit <zioneemma@gmail.com>
2023-03-21 14:49:57 +0100

    Not to commit, build environment

--- backendexpress/db.js
+++ backendexpress/db.js
@@ -4,9 +4,9 @@ let db;
 
 async function ConnectDb(cb) {
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017')
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vrce1rl.mongodb.net/?retryWrites=true&w=majority`)
     await client.connect();
-     db = client.db('blogbackend_db')
+     db = client.db('blog-backend')
     cb()
 }
 
